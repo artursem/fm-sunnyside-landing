5e7fb6ae950cdf507acaaa78b2469d40aac1dd49
artursem <artur.semeniuk@gmail.com>
2021-06-25 21:52:57 +0200

    fix js button

--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ const burger = document.querySelector('#burger');
 const nav = document.querySelector('nav');
 
 const showNav = () => {
+    // console.log('clicked!!!');
     if (nav.style.display === 'none') {
         burger.style.filter = 'opacity(0.5)';
         nav.style.display = 'flex';
@@ -11,9 +12,12 @@ const showNav = () => {
     }
 };
 
-burger.addEventListener('click', showNav);
+
 document.addEventListener('click', e => {
-    if (!nav.contains(e.target)) {
-        showNav();
+    if (!nav.contains(e.target) && !burger.contains(e.target)) {
+        burger.style.filter = 'opacity(1)';
+        nav.style.display = 'none';
     }
-})
+});
+
+burger.addEventListener('click', showNav);
